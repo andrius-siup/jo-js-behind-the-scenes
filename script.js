8eae8c13166376592db26f9609457b80edaf5c70
andrius-siup <andrius.siupinys@gmail.com>
2022-01-05 16:59:20 +0000

    Manipulated output variable and printed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function calcAge(birthYear) {
   // console.log(firstName);
 
   function printAge() {
-    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    let output = `${firstName} you are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
@@ -18,13 +18,17 @@ function calcAge(birthYear) {
       function add(a, b) {
         return a + b;
       }
+      // Error if will be declared const
+      output = 'NEW OUTPUT!';
     }
     // Error - cause const and let variables are block scope
     // console.log(str);
     console.log(millenial);
     // Error - function outside the block scope - only inside block
     // add(2, 3);
+    console.log(output);
   }
+
   printAge(); // Have to call in parent function
 
   return age;
