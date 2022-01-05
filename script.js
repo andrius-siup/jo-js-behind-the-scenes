c797956eada4f503c466720856693cc1c61fb1e3
andrius-siup <andrius.siupinys@gmail.com>
2022-01-05 15:57:59 +0000

    Created and called function in function

--- script.js
+++ script.js
@@ -2,9 +2,19 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName); // The firstName is global variable and can be executed in function
+  // The firstName is global variable and can be executed in function
+  // console.log(firstName);
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge(); // Have to call in parent function
+
   return age;
 }
 
 const firstName = 'Andrius';
 calcAge(1985);
+
+// console.log(calcAge(1985));
