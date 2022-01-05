af6987f704cc9d2624b7511cac073bbc28ea05c2
andrius-siup <andrius.siupinys@gmail.com>
2022-01-05 16:27:57 +0000

    Checked or user is millenial

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName} you are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
   }
   printAge(); // Have to call in parent function
 
